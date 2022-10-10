0c70bfd6e0b1002f3ec6e8288df0ebe2ef2628c2
Burak Yıldırım <burakyldrm0@icloud.com>
2022-10-10 12:48:50 +0300

    Bonus part completed & added Makefile bonus rule

---
+++ Src/ft_lstadd_front.c
@@ -0,0 +1,31 @@
+#include "../Lib/libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!new)
+		return ;
+	if (!lst)
+	{
+		*lst = new;
+		return ;
+	}
+	new->next = *lst;
+	*lst = new;
+}
+
+/*	AŞAĞIDAKİ KODLAR FONKSİYONU DAHA İYİ ANLAMAMIZ İÇİN YAZILMIŞTIR.	*/
+/*
+	int main()
+	{
+		t_list	*new;
+		t_list	*new2;
+		char	*str = "Hello World!";
+		char	*str2 = "Hello World2!";
+		new = ft_lstnew(str);
+		new2 = ft_lstnew(str2);
+		ft_lstadd_front(&new, new2);
+		printf("%s\n", new->content);
+		printf("%s\n", new->next->content);
+		return (0);
+	}
+*/
---
+++ Src/ft_lstiter.c
@@ -0,0 +1,12 @@
+#include "../Lib/libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
